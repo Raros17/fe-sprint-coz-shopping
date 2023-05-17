9bec49e91dc4765c3387f33490b71b4fad17dae0
Raros17 <sookmeoyhj@gmail.com>
2023-05-17 10:19:48 +0900

    mainpage title

--- fe-sprint-coz-shopping/src/Pages/Main.js
+++ fe-sprint-coz-shopping/src/Pages/Main.js
@@ -4,7 +4,11 @@ import MainStyle from '../Components/MainStyle'
 const Main = () => {
     return (
         <>
-         <MainStyle/>   
+        <section className='mainpage-section'>
+            <h2 className='main-title product-list-title'>상품 리스트</h2>
+            <h2 className='main-title bookmark-list-title'>북마크 리스트</h2>
+            <MainStyle/>            
+        </section>
         </>
     )
 }
