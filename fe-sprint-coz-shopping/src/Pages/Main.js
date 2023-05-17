e13a6b7ffc44139f6f4bf6dd31adb116452ebd44
Raros17 <sookmeoyhj@gmail.com>
2023-05-17 10:26:16 +0900

    mainpage title

--- fe-sprint-coz-shopping/src/Pages/Main.js
+++ fe-sprint-coz-shopping/src/Pages/Main.js
@@ -6,7 +6,9 @@ const Main = () => {
         <>
         <section className='mainpage-section'>
             <h2 className='main-title product-list-title'>상품 리스트</h2>
+            {/* 여기를 이제 어떻게 하지... */}
             <h2 className='main-title bookmark-list-title'>북마크 리스트</h2>
+            {/*여기를 이제 어떻게22*/}
             <MainStyle/>            
         </section>
         </>
